d85829cd62fd75de4c0f7645f8815faaa4d91084
Spencer Chastain <spencer.chastain@firstperformance.com>
2018-02-05 08:31:50 -0500

    Refactoring to share business logic in handlers

--- webrepeater.js
+++ webrepeater.js
@@ -7,21 +7,26 @@ const sio = require('socket.io');
 let server = restify.createServer();
 let io = sio.listen(server.server);
 
+function processBody(body) {
+  var ret = Object.assign({}, body);
+  ret.rcvd = new Date().toISOString();
+  return ret;
+}
+
 server.get('/alive', (req, res, next) => res.send(200, 'yes'));
 server.post('/msg',
   rp.bodyParser(),
   (req, res, next) => {
-    let body = req.body;
-    body.rcvd = new Date().toISOString();
+    let body = processBody(req.body);
     res.send(200, body);
     return undefined;
   });
 
 io.sockets.on('connection', (sock) => {
   sock.on('wsmessage', (data) => {
-    data.rcvd = new Date().toISOString();
-    data.type = 'rsp';
-    sock.emit('message', data);
+    let body = processBody(data);
+    body.type = 'rsp';
+    sock.emit('message', body);
   });
 });
 
